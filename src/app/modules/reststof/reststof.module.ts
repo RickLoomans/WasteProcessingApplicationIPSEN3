75b0b131157501700335428a01c7c86ca0a0382a
Sowi <s1126802@student.hsleiden.nl>
2022-11-29 22:55:37 +0100

    added layout, structure and homepage

---
+++ src/app/modules/reststof/reststof.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReststofVerwerkenComponent } from './components/reststof-verwerken/reststof-verwerken.component';
+
+
+
+@NgModule({
+  declarations: [
+    ReststofVerwerkenComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ReststofModule { }
