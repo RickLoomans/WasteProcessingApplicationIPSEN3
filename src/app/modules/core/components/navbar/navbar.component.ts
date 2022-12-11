98b181c69d5d1bd36931cba825813cd8441cb538
Patraklos2000 <72976276+Patraklos2000@users.noreply.github.com>
2022-12-11 14:20:24 +0100

    Feat-Navigatie
    Navigatie gemaakt en opgemaak

--- src/app/modules/core/components/navbar/navbar.component.ts
+++ src/app/modules/core/components/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import {Routes, RouterModule, Router} from '@angular/router';
+import { ReststofVerwerkenComponent } from 'src/app/modules/reststof/components/reststof-verwerken/reststof-verwerken.component';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
+
 export class NavbarComponent {
+  constructor(private router: Router){}
+  goHome(){
+    this.router.navigate(['']);
+  }
+  goRestStof(){
+    this.router.navigate(['reststofverwerken']);
+  }
+  
+  
 
 }
