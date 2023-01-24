d8630dbce53e23a0a297dd1732e52acd41a179ba
Rick Loomans <Rickloomans1@gmail.com>
2023-01-24 22:38:23 +0100

    Feat: added auth to request

--- src/app/reststof-verwerken/label-service.ts
+++ src/app/reststof-verwerken/label-service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-import {StofModel} from "./stof-model";
 
 @Injectable({providedIn: 'root'})
 export class LabelService {
@@ -8,7 +7,12 @@ export class LabelService {
   constructor(private http: HttpClient) { }
 
   fetchLabel(artikelnr: String) {
-    return this.http.get('http://localhost:8080/label/'+ artikelnr, { responseType: 'blob' })
+    const header = { 'Authorization': 'Bearer '+sessionStorage.getItem('JWT')};
+
+    return this.http.get('http://localhost:8080/label/'+ artikelnr, {
+      headers: header,
+      responseType: "blob"
+      })
   }
 
 }
